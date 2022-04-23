5765363a42387fed8d556258eed7ba74558a8314
shubhamscode <103949375+shubhamscode@users.noreply.github.com>
2022-04-23 14:03:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+// 'use strict';
+// console.log("hello");
+
+
+// const num1 = parseInt(prompt('Enter the first number '));
+// const num2 = parseInt(prompt('Enter the second number '));
+
+// const sum = num1 + num2;
+
+// console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+
+
+
+
+// let P = 1, R = 1, T = 1;
+ 
+// let SI = (P * T * R) / 100;
+
+// console.log("Simple Interest = " + SI);
+
+
+
+
+
+// function kE(M, V, H)
+// {
+       
+//     let KineticEnergy;
+//     KineticEnergy = 0.5 * M * V * H;
+//         return KineticEnergy;
+// }
+
+//     let M = 5.5, H = 23.5, V = 10.5;
+//     console.log("Kinetic Energy = "+kE(M, V, H));
+
+
+
+// function areaofSide(a)
+// {
+//     return (a*a);
+// }
+ 
+// function perimeter(a)
+// {
+//     return (4*a);
+// }
+// function surfaceArea(a)
+// {
+//     return (6*a*a);
+// }
+// function volume(a)
+// {
+//     return (a*a*a);
+// }
+// let a=5;
+// console.log("Area Of Side is "+areaofSide(a));
+// console.log("Perimeter is "+perimeter(a));
+// console.log("Surface Area is "+surfaceArea(a));
+// console.log("Volume is "+volume(a));
+
+
+
+
+
+
+// const number = (prompt('Enter a positive integer: '));
+
+// let sum = 0;
+// for (let i = 1; i <= number; i++) {
+//     sum += i;
+// }
+// console.log('The sum of natural numbers:', sum);
+
+
+
+
+
+// let n = parseInt(prompt("Enter a number:"));
+
+// for (i = n - 1; i >= 0; i--) {
+//   a = 1 + i * 2;
+//   console.log(a);
+// }
+
+
+
+// let i, n=4, sum=0;
+//   arr = new Array(1, 2, 3, 4);
+//   for(i=0; i<4; i++)
+//     sum = sum + arr[i];
+//   console.log(sum);
+
+
+
+// function reverseNumber(n)
+// {
+// 	n = n + " ";
+// 	return n.split("").reverse().join("");
+// }
+// console.log(Number(reverseNumber(32243)));
+
+
+
+
+
+// const number = parseInt(prompt('Enter a decimal number: '));
+
+// const result = number.toString(2);
+
+// console.log('Binary:' + ' ' + result);
+
+
+
+
+
+const octal = "116";
+
+console.log(parseInt(octal,8));
